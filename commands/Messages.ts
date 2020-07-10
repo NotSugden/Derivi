a0b31fa0cbf1c83b766f249a3b6d316091bfb6dc
NotSugden <28943913+NotSugden@users.noreply.github.com>
2020-07-10 02:27:34 +0100

    fix(commands/Messages): correctly resolve time

--- commands/Messages.ts
+++ commands/Messages.ts
@@ -3,7 +3,7 @@ import { Snowflake } from 'discord.js';
 import Command, { CommandData } from '../structures/Command';
 import CommandArguments from '../structures/CommandArguments';
 import CommandManager from '../util/CommandManager';
-import { Responses } from '../util/Constants';
+import { Responses, SNOWFLAKE_REGEX } from '../util/Constants';
 import { GuildMessage } from '../util/Types';
 import Util from '../util/Util';
 
@@ -22,15 +22,19 @@ export default class Messages extends Command {
 
 	public async run(message: GuildMessage<true>, args: CommandArguments, { send }: CommandData) {
 		const user = await Util.users(message, 1) || message.author;
-
-		const _args = (args[1] ? args.slice(1) : args).join(' ');
-		let time = _args === 'alltime' ? 0 : Util.parseMS(_args);
-		if (time === -1) time = 6048e5;
+		const isMention = SNOWFLAKE_REGEX.test(args.regular[0]);
+		let time;
+		if ((args[1] || args[0]) === 'alltime') time = new Date(0);
+		else {
+			time = Util.parseMS((isMention ? args.slice(1) : args).join(' '));
+			if (time === -1) time = 6048e5;
+			time = new Date(Date.now() - time);
+		}
 
 		const messages = await this.client.database.query<{ count: number; channelID: Snowflake }>(
 			'SELECT COUNT(*) as count, channel_id as channelID FROM messages \
 WHERE sent_timestamp > :after AND user_id = :userID AND guild_id = :guildID GROUP BY channel_id ORDER BY count DESC', {
-				after: new Date(Date.now() - time),
+				after: time,
 				guildID: message.guild.id,
 				userID: user.id
 			}
@@ -38,6 +42,6 @@ WHERE sent_timestamp > :after AND user_id = :userID AND guild_id = :guildID GROU
 		return send(Responses.MESSAGES({
 			channels: messages,
 			total: messages.reduce((acc, next) => acc + next.count, 0)
-		}, user, ms(time, true)));
+		}, user, ms(Date.now() - time.getTime(), true)));
 	}
 }
